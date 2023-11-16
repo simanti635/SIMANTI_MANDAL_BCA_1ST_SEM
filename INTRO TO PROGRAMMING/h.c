1ccd3a88ed3dde974f3b581fa01dd1dfe5c51013
simanti635 <144654809+simanti635@users.noreply.github.com>
2023-11-16 21:38:04 +0530

    Add files via upload

---
+++ INTRO TO PROGRAMMING/h.c
@@ -0,0 +1,8 @@
+//  WAP IN C TO PRINT THE ASCII VALUE OF CHARACTER
+#include<stdio.h>
+int main(){
+    char ch;
+    printf("ENTER A CHARACTER : ");
+    scanf("%c",&ch);
+    printf("THE ASCII VALUE OF %c IS %d \n",ch,ch);
+}
