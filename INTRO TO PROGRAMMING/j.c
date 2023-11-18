35778fd84c4eaad78f67bf797bfc81e5db306ab9
simanti635 <144654809+simanti635@users.noreply.github.com>
2023-11-18 21:30:27 +0530

    Add files via upload

---
+++ INTRO TO PROGRAMMING/j.c
@@ -0,0 +1,10 @@
+// WAP IN C TO FIND THE AGE BY TAKING THE YEAR OF BIRTH BY USER
+#include<stdio.h>
+int main(){
+    int yob;
+    printf("ENTER THE YEAR OF BIRTH : ");
+    scanf("%d", &yob);
+    printf("YOUR AGE IS %d", 2023-yob);
+
+    
+    }
